60451712c81429a833dd4b32a5d7d595fc335b9e
Dan <feldd558@newschool.edu>
2016-03-17 09:05:25 -0400

    create quiz 3

--- lab-js/assets/js/app.js
+++ lab-js/assets/js/app.js
@@ -1,1 +1,20 @@
-alert('hello js');
+//alert('hello js');
+
+var animal = "cat";
+var age = 12;
+var clothing = ["shirts", "socks", "pants"];
+var winner = false;
+var checking_account = 1000;
+
+console.log(winner);
+
+
+function print_text(text){
+	$('.value').html(text);
+
+}
+
+function charge_cc(item) {
+
+}
+
